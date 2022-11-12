570c8fefecc97dc7b58dc4b5ff955ed1d5f8b369
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-11-12 20:37:38 +0100

    code optimalization

--- types/postsTypes.ts
+++ types/postsTypes.ts
@@ -1,3 +1,6 @@
+// import { Content } from 'mdast';
+// import React, { ReactNode, ReactElement } from 'react';
+
 export interface PostItemType {
   slug: string;
   data: { title: string; image: string; excerpt: string; date: string };
@@ -11,3 +14,15 @@ export type PostsProps = {
 export type PostProps = {
   post: PostItemType;
 };
+
+// type NodeToProps<T> = {
+//   node: T;
+//   children: T extends { children: any } ? ReactNode : never;
+//   className?: string;
+// };
+
+// export type CustomRenderers = {
+//   [K in Content['type']]?: (
+//     props: NodeToProps<Extract<Content, { type: K }>>
+//   ) => ReactElement;
+// };
