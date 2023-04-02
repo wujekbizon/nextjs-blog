d091cdecc845d0b02e655af655f2ad36ed6097cc
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-04-02 14:04:49 +0200

    small changes to css

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,16 +1,11 @@
-import styles from './Logo.module.css';
-import Image from 'next/image';
+import styles from './Logo.module.css'
+import Image from 'next/image'
 
 const Logo = () => {
   return (
     <div className={styles.logo}>
-      <Image
-        src="/images/site/logo.png"
-        alt="wolfinger"
-        width={120}
-        height={40}
-      />
+      <Image src="/images/site/logo.png" alt="wolfinger" width={120} height={40} />
     </div>
-  );
-};
-export default Logo;
+  )
+}
+export default Logo
