e84d9b4686ce48d65c99c895727eb6e2541581e8
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-02-22 07:01:49 +0100

    Fix commas error

--- components/home-page/Explore.tsx
+++ components/home-page/Explore.tsx
@@ -4,11 +4,11 @@ import styles from './Explore.module.css'
 const Explore = () => {
   return (
     <section className={styles.explore}>
-      <h1 className={styles.explore_title}>Check what's coming in near future!</h1>
+      <h1 className={styles.explore_title}>Check whats coming in near future!</h1>
       <AnimatedSquare>
         <h1>Animation - Framer Motion - CSS</h1>
         <p>
-          Bring your designs to life with Framer's fluid animations and CSS's powerful styling, crafting experiences as
+          Bring your designs to life with Framer fluid animations and CSS powerful styling, crafting experiences as
           dynamic as they are beautiful.
         </p>
         <p>New Series Articles, Coming Soon!</p>
